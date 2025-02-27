f3137195690fae2d011e66bd37e3363c0a89a243
inundata <james.minho.lee@gmail.com>
2025-02-28 06:54:32 +0900

    Initial commit

---
+++ _posts/upload-data/pull_and_deploy.bat
@@ -0,0 +1,5 @@
+@echo off
+chcp 65001 >nul
+call db-pull.bat
+"C:\Program Files\Git\bin\bash.exe" -c "/e/OneDrive/Github/inundata.github.io/_posts/upload-data/deploy.sh"
+pause
---
+++ reset_github_repo.bat
@@ -0,0 +1,3 @@
+chcp 65001 >nul
+"C:\Users\HERO\anaconda3\envs\db\python.exe" "E:\OneDrive\Github\inundata.github.io\reset_github_repo.py"
+pause
