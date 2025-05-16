9a5fd407bad5d2d3f4756a4e46c0213891819ed3
inundata <james.minho.lee@gmail.com>
2025-05-16 10:28:05 +0900

    자동 업데이트: Fri May 16 10:28:05     2025

--- _posts/upload-data/deploy.sh
+++ _posts/upload-data/deploy.sh
@@ -22,10 +22,10 @@ cd "/e/OneDrive/Github/inundata.github.io" || exit
 git add .
 git commit -m "자동 업데이트: $(date)"
 
-# Git LFS 및 히스토리 정리
-git lfs prune
-git reflog expire --expire=now --all
-git gc --prune=now
+# # Git LFS 및 히스토리 정리
+# git lfs prune
+# git reflog expire --expire=now --all
+# git gc --prune=now
 
 # GitHub로 Push
 git push -u origin main
